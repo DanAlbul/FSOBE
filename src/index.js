1e912044e1089a6a48d05815a2bd2245917ffe95
DanAlbul <daniel.albul@dochq.co.uk>
2024-01-03 20:36:58 +0200

    update backend routes to fetch all data from mongo database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors'
+import dotenv from 'dotenv';
+import Person from './models/person.js'
 
 const app = express();
+dotenv.config();
 
 const customMorganFormat = ':method :url :status :res[content-length] - :response-time ms :data';
 morgan.token('data', (req) => {
@@ -21,18 +24,8 @@ app.listen(PORT, () => {
 	console.log(`Server running at ${hostname}/`);
 });
 
-const getInfo = () => {
-	const entries = Object.entries(persons)
-	const today = new Date()
-	return (
-		`<div>
-			<p>Phonebook has info for ${entries.length} people</p>
-			<p>${today}</p>
-		</div>`
-	)
-}
 
-let persons = [
+/* let persons = [
     {
       "id": 1,
       "name": "Arto Hellas",
@@ -54,63 +47,65 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
-
+ */
 
 app.get('/', (req, res) => {
 	return res.send('<h1>Phonebook</h1>');
 });
 
 app.get('/api/persons', (req, res) => {
-	return res.status(200).json(persons)
+	Person.find({}).then(persons => {
+    return res.status(200).json(persons).end()
+  })
 });
 
 app.get('/info', (req, res) => {
-	const infoPage = getInfo()
-	return res.send(infoPage)
-})
+	Person.find({}).then(persons => {
+		const entries = Object.entries(persons)
+		const today = new Date()
+		const infoPage = `<p>Phonebook has info for ${entries.length} people</p><p>${today}</p>`
+		return res.send(infoPage).end()
+	})
+});
 
 
 app.get('/api/persons/:id', (req, res) => {
-	const id = Number(req.params.id)
-	if(isNaN(id)) return res.status(400).send('Invalid id').end()
-	const number = persons.find(p => p.id === id)
-	if (!number) return res.status(404).send(`Number by id ${id} is not found`).end()
-	return res.json(number).end()
+	Person.findById(req.params.id).then(person => {
+		if (person) {
+			return res.json(person).end()
+		} else {
+			return res.status(404).send(`Number by id ${id} is not found`).end()
+		}
+	})
 })
 
 
 app.delete('/api/persons/:id', (req, res) => {
-	const id = Number(req.params.id)
-	if(isNaN(id)) return res.status(400).send('Invalid id').end()
-  if (!persons.some(p => p.id === id))
-		return res.status(404).send(`Number by id ${id} is not found`).end()
-
-	persons = persons.filter(p => p.id !== id)
-	return res.status(204).send(`Person with id: ${id} was deleted successfully`).end()
+	console.log(req.params.id)
+	Person.findByIdAndDelete(req.params.id).then(person => {
+		if (person === null) return res.status(404).send(`Number by id ${req.params.id} is not found`).end()
+		else return res.status(204).send(`Person with id: ${req.params.id} was deleted successfully`).end()
+	}).catch(error => {
+		console.log(error)
+		return res.status(500).end()
+	})
 })
 
 app.post('/api/persons', (req, res) => {
 	const data = req.body
+	const newNumber = new Person({
+		name: data.name,
+		number: data.number,
+	})
 
 	if(Object.values(data).some(v => v === ''))
-		return res.status(400).json({error: 'Some content is missing!'}).end()
+		return res.json({error: 'Some content is missing!'}).end()
 
 	if(!isNaN(data.name))
-		return res.status(400).json({error: 'Name must be a string!'}).end()
+		return res.json({error: 'Name must be a string!'}).end()
 
 	if(isNaN(data.number))
-		return res.status(400).json({error: 'Number must consists of digits!'}).end()
+		return res.json({error: 'Number must consists of digits!'}).end()
 
-	if(persons.some(person => person.name === data.name))
-		return res.status(400).json({error: 'Name must be unique!'}).end()
-
-	if(persons.some(person => person.number === data.number))
-		return res.status(400).json({error: 'Number must be unique!'}).end()
-
-	const newNumber = {
-		...data,
-		id: Math.floor(Math.random() * 1000000000)
-	}
-	persons = persons.concat(newNumber)
-	return res.json(newNumber).end()
+	newNumber.save().then(p => res.json(p).end())
 })
---
+++ src/models/person.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to MongoDB...')
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+//-----------------------PERSON---------------------------------
+// define the person schema and create a model
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+// change the format of the returned object to remove the _id and __v properties and replace _id with id
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+const Person = mongoose.model('Person', personSchema)
+
+export default Person
