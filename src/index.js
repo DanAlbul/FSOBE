691804504d4d1279a32e3be8dd12f479984b38da
DanAlbul <daniel.albul@dochq.co.uk>
2024-01-03 20:37:30 +0200

    remove old persons array

--- src/index.js
+++ src/index.js
@@ -25,30 +25,6 @@ app.listen(PORT, () => {
 });
 
 
-/* let persons = [
-    {
-      "id": 1,
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": 2,
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": 3,
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": 4,
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
-]
- */
-
 app.get('/', (req, res) => {
 	return res.send('<h1>Phonebook</h1>');
 });
